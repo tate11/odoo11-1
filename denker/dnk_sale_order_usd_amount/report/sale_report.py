dff2cabdb1b695018e7aee876464af75d5942702
Benjamín Cruz <bcruz@grupodenker.com>
2018-09-21 17:23:42 -0500

    [dnk_sale_order_usd_amount] Se corrige el monto en USD en reporte y se agrega color y familia.

--- denker/dnk_sale_order_usd_amount/report/sale_report.py
+++ denker/dnk_sale_order_usd_amount/report/sale_report.py
@@ -8,6 +8,28 @@ class SaleReport(models.Model):
     _inherit = 'sale.report'
 
     dnk_price_subtotal_usd = fields.Float('- Untaxed Total USD')
-    
+    dnk_family =fields.Char('- Family')
+    dnk_color =fields.Char('- Color')
+
     def _select(self):
-        return super(SaleReport, self)._select() + ",sum(l.dnk_price_subtotal_usd / COALESCE(cr.rate, 1.0)) as dnk_price_subtotal_usd "
+        select_subtotal_usd = ",sum(l.dnk_price_subtotal_usd) as dnk_price_subtotal_usd "
+        select_family_color = ",pc.family as dnk_family, pc.color as dnk_color"
+        return super(SaleReport, self)._select() + select_subtotal_usd + select_family_color
+
+    def _from(self):
+        select_add_family = """
+            LEFT JOIN
+                (SELECT subfamily.id, subfamily.name AS  subfamily, family.name AS family, color.name AS color FROM product_category subfamily
+			LEFT JOIN
+			(SELECT pc2.id, pc2.name, pc2.parent_id FROM product_category pc2 WHERE pc2.parent_id IN
+                (SELECT id FROM product_category WHERE parent_id IS NULL)) family  ON family.id = subfamily.parent_id
+			LEFT JOIN
+			(SELECT pc3.id,pc3.name FROM product_category pc3 WHERE pc3.parent_id IS NULL) color ON family.parent_id = color.id
+				WHERE subfamily.parent_id IS NOT NULL AND subfamily.parent_id NOT IN (SELECT id FROM product_category WHERE parent_id IS NULL))
+            AS pc ON pc.id = t.categ_id
+        """
+        return super(SaleReport, self)._from() + select_add_family
+
+    def _group_by(self):
+        dnk_group_by = ",pc.family, pc.color"
+        return super(SaleReport, self)._group_by() + dnk_group_by
