449d0eead801d49681bcae7e4c3b61adb29dd182
Benjamín Cruz <bcruz@grupodenker.com>
2018-10-17 17:48:32 -0500

    [dnk_sale_purchase_order_file] Otros cambios, ninguno significativo, pero no sé ni qué pedo  :(

--- denker/dnk_sale_purchase_order_file/models/sale_order.py
+++ denker/dnk_sale_purchase_order_file/models/sale_order.py
@@ -5,13 +5,12 @@ from odoo.exceptions import UserError, RedirectWarning, ValidationError
 class PurchaseOrderFile(models.Model):
     _inherit = ['sale.order']
 
+    dnk_purchase_order_name = fields.Char('- Purchase Order Name')
+    dnk_purchase_order_file = fields.Binary('- Purchase Order File', store=True)
+
     @api.model
     def action_confirm(self):
         res =super(PurchaseOrderFile, self).action_confirm()
-        if self.partner_id.dnk_purchase_order_required and not self.dnk_purchase_order_file:
+        if self.partner_id.dnk_purchase_order_required and not self.dnk_purchase_order_name :
             raise ValidationError("The settings for this client requires a purchase order file.")
         return res
-
-
-    dnk_purchase_order_name = fields.Char('- Purchase Order Name')
-    dnk_purchase_order_file = fields.Binary('- Purchase Order File', attachment=True, store=True)
