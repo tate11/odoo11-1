2c35a551f8a20bca6d594a561640f3da0435e197
José Candelas <jcandelas@grupodenker.com>
2018-10-30 12:56:48 -0600

    Actualización del módulo dnk_sale_theoretical_cost

--- denker/dnk_sale_theoretical_cost/models/product.py
+++ denker/dnk_sale_theoretical_cost/models/product.py
@@ -6,11 +6,18 @@ from odoo import models, fields, api
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
+    @api.model
+    def _default_currency(self):
+        return self.env.user.company_id.currency_id
+
+
     dnk_theoretical_cost = fields.Float(
         string="- Theoretical Cost",
         compute='_compute_theoretical_cost',
         inverse='_set_theoretical_cost',
         digits=dp.get_precision('Product Price'), groups="base.group_user")
+
+    dnk_theoretical_cost_currency_id = fields.Many2one('res.currency', string='- Theoretical Cost Currency', default=_default_currency)
 
 
     @api.one
@@ -22,10 +29,13 @@ class ProductTemplate(models.Model):
     @api.depends('product_variant_ids', 'product_variant_ids.standard_price')
     def _compute_theoretical_cost(self):
         unique_variants = self.filtered(lambda template: len(template.product_variant_ids) == 1)
+
         for template in unique_variants:
             template.dnk_theoretical_cost = template.product_variant_ids.dnk_theoretical_cost
+            template.dnk_theoretical_cost_currency_id = template.product_variant_ids.dnk_theoretical_cost_currency_id
         for template in (self - unique_variants):
             template.dnk_theoretical_cost = 0.0
+            template.dnk_theoretical_cost_currency_id = template.product_variant_ids.dnk_theoretical_cost_currency_id
 
 
 class ProductProduct(models.Model):
@@ -35,3 +45,15 @@ class ProductProduct(models.Model):
         string="- Theoretical Cost",
         company_dependent=True,
         digits=dp.get_precision('Product Price'), groups="base.group_user")
+
+    dnk_theoretical_cost_currency_id = fields.Many2one('res.currency', string='- Theoretical Cost Currency', related='product_tmpl_id.dnk_theoretical_cost_currency_id')
+
+    dnk_theoretical_cost_converted = fields.Float(
+        string="- Theoretical Cost Converted",
+        compute='compute_theorerical_cost',
+        digits=dp.get_precision('Product Price'), groups="base.group_user")
+
+    @api.one
+    @api.depends('dnk_theoretical_cost')
+    def compute_theorerical_cost(self):
+        self.dnk_theoretical_cost_converted =  self.env['res.currency']._compute(self.dnk_theoretical_cost_currency_id, self.env.user.company_id.currency_id, self.dnk_theoretical_cost)
