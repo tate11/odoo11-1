a7c840c0029e9fe0178802d53134fd716ce18f12
Benjamín Cruz <bcruz@grupodenker.com>
2018-11-12 12:05:15 -0600

    [credit_limit_alert] Nueva versión :)

--- custom/credit_limit_alert/models/sale_order.py
+++ custom/credit_limit_alert/models/sale_order.py
@@ -2,6 +2,17 @@ from odoo import models, fields, api, exceptions,_
 from datetime import datetime, date, time, timedelta
 import calendar
 
+class SaleConfigSettings(models.Model):
+    _inherit = "res.company"
+
+    inmediate_payment = fields.Many2one('account.payment.term',string="Termino de pago inmediato")
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = "res.config.settings"
+
+    inmediate_payment = fields.Many2one('account.payment.term',string="Termino de pago inmediato", related='company_id.inmediate_payment')
+
 class CreditLimitAlertSaleOrder(models.Model):
     _name = 'sale.order'
     _inherit = 'sale.order'
@@ -14,29 +25,44 @@ class CreditLimitAlertSaleOrder(models.Model):
     def action_confirm(self):
         current_date = date.today() - timedelta(days=15)
         cr = self.env.cr
-        cr.execute("select COALESCE(SUM(1),0) FROM account_invoice WHERE type='out_invoice' AND state='open' AND date_due<='"+str(current_date)+"' AND partner_id='"+str(self.partner_id.id)+"'")
+
+        sql = ""
+
+        if self.partner_id.parent_id:
+            sql = "select COALESCE(SUM(1),0) FROM account_invoice WHERE type='out_invoice' AND state='open' AND date_due<='"+str(current_date)+"' AND (partner_id='"+str(self.partner_id.id)+"' OR partner_id='"+str(self.partner_id.parent_id.id)+"')"
+        else:
+            sql = "select COALESCE(SUM(1),0) FROM account_invoice WHERE type='out_invoice' AND state='open' AND date_due<='"+str(current_date)+"' AND partner_id='"+str(self.partner_id.id)+"'"
+
+        cr.execute(sql)
         facturas_vencidas = cr.fetchone()
         fac = max(facturas_vencidas)
-
+        pago_in = self.company_id.inmediate_payment.id
 
         if self.partner_id.block_sales==True:
             raise exceptions.ValidationError('El cliente tiene bloqueadas las ventas')
 
+        if self.partner_id.parent_id.block_sales==True:
+            raise exceptions.ValidationError('La compañia del cliente tiene bloqueadas las ventas')
+
         if not self.payment_term_id:
             raise exceptions.ValidationError('Necesita seleccionar un plazo de pago.')
 
-        if (self.payment_term_id.id==1 and self.paid_sale_order!=True):
+        if (self.payment_term_id.id==pago_in and self.paid_sale_order!=True):
             raise exceptions.ValidationError('Si el pago es inmediato necesita validar que esta orden esté pagada')
 
-        if (fac >= 1 and self.payment_term_id.id != 1 and self.permitted_credit_limit is not True):
-            raise exceptions.ValidationError('Este cliente cuenta con facturas vencidas.')
+        if (fac >= 1 and self.payment_term_id.id != pago_in and self.permitted_credit_limit is not True):
+            raise exceptions.ValidationError('Este cliente o la empresa a la que pertenece tiene facturas vencidas.')
         else:
 
             if self.partner_id.credit_limit != 0:
-                credit = self.env['res.currency']._compute(self.partner_id.currency_id,self.currency_id,self.partner_id.credit)
-                credit_limit = self.env['res.currency']._compute(self.partner_id.currency_id,self.currency_id,self.partner_id.credit_limit)
+                credit = self.env['res.currency']._compute(self.company_id.currency_id,self.currency_id,self.partner_id.credit)
+                credit_limit = self.env['res.currency']._compute(self.company_id.currency_id,self.currency_id,self.partner_id.credit_limit)
+                c_credit = self.env['res.currency']._compute(self.company_id.currency_id,self.currency_id,self.partner_id.parent_id.credit)
+                c_credit_limit = self.env['res.currency']._compute(self.company_id.currency_id,self.currency_id,self.partner_id.credit_limit)
+
+                
                 if credit + self.amount_total > credit_limit:
-                    if self.payment_term_id.name != 'Immediate Payment':
+                    if self.payment_term_id.id != pago_in:
                         if self.permitted_credit_limit is not True:
                             self.avisado = True
                             raise exceptions.ValidationError('Este cliente ha exedido el limite de credito. Su limite actual es: '
@@ -44,6 +70,17 @@ class CreditLimitAlertSaleOrder(models.Model):
                                                              + str(self.partner_id.credit) + ' y disponible tiene '
                                                              + str(self.partner_id.credit_available)
                                                              + ', debe de autorizar el limite de credito excedido' )
+
+                if c_credit + self.amount_total > c_credit_limit:
+                    if self.payment_term_id.id != pago_in:
+                        if self.permitted_credit_limit is not True:
+                            self.avisado = True
+                            raise exceptions.ValidationError('La compañia del cliente ha exedido el limite de credito. Su limite actual es: '
+                                                             + str(self.partner_id.parent_id.credit_limit) +', actualmente tiene una deuda de: '
+                                                             + str(self.partner_id.parent_id.credit) + ' y disponible tiene '
+                                                             + str(self.partner_id.parent_id.credit_available)
+                                                             + ', debe de autorizar el limite de credito excedido' )
+
 
             res = super(CreditLimitAlertSaleOrder, self).action_confirm()
 
@@ -61,11 +98,19 @@ class CreditLimitAlertStockPicking(models.Model):
         self.ensure_one()
         current_date = date.today() - timedelta(days=15)
         cr = self.env.cr
-        cr.execute("select COALESCE(SUM(1),0) FROM account_invoice WHERE type='out_invoice' AND state='open' AND date_due<='"+str(current_date)+"' AND partner_id='"+str(self.partner_id.id)+"'")
+
+        sql = ""
+
+        if self.partner_id.parent_id:
+            sql = "select COALESCE(SUM(1),0) FROM account_invoice WHERE type='out_invoice' AND state='open' AND date_due<='"+str(current_date)+"' AND (partner_id='"+str(self.partner_id.id)+"' OR partner_id='"+str(self.partner_id.parent_id.id)+"')"
+        else:
+            sql = "select COALESCE(SUM(1),0) FROM account_invoice WHERE type='out_invoice' AND state='open' AND date_due<='"+str(current_date)+"' AND partner_id='"+str(self.partner_id.id)+"'"
+
+        cr.execute(sql)
         facturas_vencidas = cr.fetchone()
         fac = max(facturas_vencidas)
-        if fac >= 1 and self.allow_delivery is not True:
-            raise exceptions.ValidationError('Este cliente cuenta con facturas vencidas.')
+        if fac >= 1 and self.allow_delivery is not True and self.picking_type_code == 'outgoing':
+            raise exceptions.ValidationError('Este cliente o la empresa a la que pertenece tiene facturas vencidas.')
         else:
 
             res = super(CreditLimitAlertStockPicking, self).button_validate()
